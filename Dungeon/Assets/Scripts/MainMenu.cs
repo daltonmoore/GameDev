0423aaca4d3dc2745d2b5c7eb7a8be14753bb851
Dalton Moore <daltonraymoore@gmail.com>
2019-07-23 22:39:53 -0400

    First Commit

---
+++ Dungeon/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    Button playGame;
+
+    void Start()
+    {
+        playGame = GameObject.Find("Play Game Button").GetComponent<Button>();
+        playGame.onClick.AddListener(ClickPlayGameButton);
+    }
+    
+    void ClickPlayGameButton()
+    {
+        SceneManager.LoadScene("Start");
+    }
+}
