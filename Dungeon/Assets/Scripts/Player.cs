739e397d1ef5e6d5cc7cbdc8cfd5728cf9b66630
Dalton Moore <daltonraymoore@gmail.com>
2019-07-24 22:44:11 -0400

    changed soil sizes to 64 by 64, added simple player movement, added rough draft player sprite

---
+++ Dungeon/Assets/Scripts/Player.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField]
+    float movespeed;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        move();
+    }
+
+    void move()
+    {
+        if(Input.GetKey(KeyCode.D))
+        {
+            transform.position = new Vector3(transform.position.x + movespeed, transform.position.y);
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            transform.position = new Vector3(transform.position.x - movespeed, transform.position.y);
+        }
+        else if (Input.GetKey(KeyCode.W))
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y + movespeed);
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y - movespeed);
+        }
+    }
+}
