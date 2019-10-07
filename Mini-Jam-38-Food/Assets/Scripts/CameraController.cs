78ab05602851c5db49dbc945a83a5c1922eb46c3
Dalton Moore <daltonraymoore@gmail.com>
2019-10-06 22:46:32 -0400

    first commit

---
+++ Mini-Jam-38-Food/Assets/Scripts/CameraController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    GameObject player;
+    Vector3 offset = new Vector3(0, .5f, -10);
+
+    void Start()
+    {
+        player = GameObject.Find("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = player.transform.position + offset;
+    }
+}
---
+++ Mini-Jam-38-Food/Assets/Scripts/Player.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class Player : MonoBehaviour
+{
+    CircleCollider2D circleCollider;
+    public bool grounded = true;
+    public float moveForce, jumpForce;
+    Rigidbody2D rigid;
+
+    void Start()
+    {
+        circleCollider = GetComponent<CircleCollider2D>();
+        rigid = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        move();
+    }
+
+    void move()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            rigid.AddForce(Vector2.left * moveForce * Time.deltaTime, ForceMode2D.Force);
+        }
+        else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            rigid.AddForce(Vector2.right * moveForce * Time.deltaTime);
+        }
+    }
+}
