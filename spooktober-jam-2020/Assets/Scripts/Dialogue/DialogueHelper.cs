4f8b8ec42fccd8b5fd8b55f0c478b223836b781b
daltonmoore <daltonraymoore@gmail.com>
2020-10-25 00:04:28 -0400

    initial commit spooktober-jam

---
+++ spooktober-jam-2020/Assets/Scripts/Dialogue/DialogueHelper.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class DialogueHelper
+{
+    static GameObject CurrentNPC;
+    static bool PlayerIsAbleToOpenDialogue;
+
+
+    public static void NPCDialogueTriggerFired(GameObject source)
+    {
+        if (CurrentNPC == null)
+        {
+            CurrentNPC = source;
+            PlayerIsAbleToOpenDialogue = true;
+        }
+    }
+
+}
